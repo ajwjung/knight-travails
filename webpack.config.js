9e50352cbfdb07d2dd31a71a29997bb77ebe526b
Amy Jung <ajwjung@outlook.com>
2023-04-06 11:46:31 -0700

    Set up eslint and reformat files

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,16 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Knight Travails",
-            template: "./src/index.html",
-        })
-    ],
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Knight Travails",
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
 };
