1406dfecf5ade2de416322e750a3754aa76dd0d5
Amy Jung <ajwjung@outlook.com>
2023-04-07 11:44:16 -0700

    Create function to get shortest path

--- src/modules/knight-travails.js
+++ src/modules/knight-travails.js
@@ -32,12 +32,51 @@ const KnightTravails = (() => {
                 coord[1] > 0 && coord[1] < 9)
 
             return { adjList: filteredList, coordinates };
-        })
+        });
 
         return graph;
     };
 
-    return { boardCoordinates, createAdjacencyList };
+    function formatPath(path) {
+        const formattedPath = path.map(square => `\t[${square}]\n`).join("");
+        
+        return `=> You made it in ${path.length - 1} moves! Here's your path: \n${formattedPath}`;
+    };
+
+    function checkArrayPresent(sourceArr, searchArr) {
+        for (const coordinates of sourceArr) {
+            return (coordinates[0] === searchArr[0] &&
+                coordinates[1] === searchArr[1])
+        }
+    }
+
+    function getShortestPath(start, end) {
+        const queue = [{ square: start, path: [start] }];
+        const visited = [];
+
+        while (queue.length > 0) {
+            const { square, path } = queue.shift();
+
+            if (square[0] === end[0] && square[1] === end[1]) {
+                return formatPath(path);
+            }
+
+            const adjacencyList = createAdjacencyList().find(position => 
+                ((position.coordinates[0] === square[0]) &&
+                (position.coordinates[1] === square[1]))
+            )
+
+            adjacencyList.adjList.forEach(nextSquare => {
+                if (!(checkArrayPresent(visited, nextSquare))) {
+                    visited.push(nextSquare);
+                    const newPath = path.concat([nextSquare]);
+                    queue.push({ square: nextSquare, path: newPath });
+                }
+            })
+        }
+    }
+
+    return { boardCoordinates, getShortestPath };
 })();
 
 export default KnightTravails;
